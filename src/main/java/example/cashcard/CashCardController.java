eee70945df7fe9d330c14ada4ff4c3490dc62270
Max <85370431+maxsplawski@users.noreply.github.com>
2025-02-18 20:13:43 +0100

    chore: update CashCardController

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    
     private final CashCardRepository cashCardRepository;
 
     public CashCardController(CashCardRepository cashCardRepository) {
